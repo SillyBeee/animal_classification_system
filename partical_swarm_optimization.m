e2d6280cdbdb5009d58dcb00b946bacf5df42a82
HuaCheng Ma <3576527475a@gmail.com>
2024-12-29 16:10:27 +0800

    修改PSO

--- partical_swarm_optimization.m
+++ partical_swarm_optimization.m
@@ -11,19 +11,19 @@ w = 0.7;          % 惯性权重
 c1 = 1.5;         % 个体学习因子
 c2 = 1.5;         % 群体学习因子
 
-% 初始化粒子群
+
 x = lb + (ub-lb) * rand(pop_size, dim);  % 位置
 v = zeros(pop_size, dim);                % 速度
 p_best = x;                              % 个体最佳位置
 g_best = x(1, :);                        % 全局最佳位置
 fitness = @(x1, x2) 15 + x1.^2 + x2.^2 - 10*(cos(2*pi*x1) + cos(2*pi*x2));
 
-% 计算初始适应度
-p_best_val = arrayfun(@(i) fitness(x(i, 1), x(i, 2)), 1:pop_size);
-[g_best_val, g_best_idx] = min(p_best_val); % 改为max(p_best_val)求最大值
+
+p_best_val = arrayfun(@(i) fitness(x(i, 1), x(i, 2)), 1:pop_size);% 计算初始适应度
+[g_best_val, g_best_idx] = max(p_best_val); 
 g_best = p_best(g_best_idx, :);
 
-% PSO迭代
+
 for iter = 1:max_iter
     for i = 1:pop_size
         % 更新速度和位置
@@ -36,23 +36,23 @@ for iter = 1:max_iter
 
         % 更新个体最佳
         f_val = fitness(x(i, 1), x(i, 2));
-        if f_val < p_best_val(i)  % 改为 > 求最大值
+        if f_val > p_best_val(i)  
             p_best(i, :) = x(i, :);
             p_best_val(i) = f_val;
         end
     end
 
-    % 更新全局最佳
-    [current_best_val, current_best_idx] = min(p_best_val); % 改为 max 求最大值
-    if current_best_val < g_best_val  % 改为 > 求最大值
+
+    [current_best_val, current_best_idx] = max(p_best_val);     % 更新全局最佳
+    if current_best_val > g_best_val  
         g_best_val = current_best_val;
         g_best = p_best(current_best_idx, :);
     end
 
-    % 输出迭代信息
-    fprintf('第%d次迭代: 最优值 = %.4f\n', iter, g_best_val);
+
+    fprintf('第%d次迭代: 最优值 = %.4f\n', iter, g_best_val);     % 输出迭代信息
 end
 
-% 输出结果
-fprintf('最优解: x1 = %.4f, x2 = %.4f\n', g_best(1), g_best(2));
+
+fprintf('最优解: x1 = %.4f, x2 = %.4f\n', g_best(1), g_best(2)); % 输出结果
 fprintf('最优值: %.4f\n', g_best_val);
